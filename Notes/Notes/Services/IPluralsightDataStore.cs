3167eae9793a5d66d615a24710d425d052d9291d
Kris <kris_s2@outlook.com>
2022-03-21 23:48:21 -0700

    Preparing to add new binding source. Adding Note class and new interface for data storage;

---
+++ Notes/Notes/Services/IPluralsightDataStore.cs
@@ -0,0 +1,17 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Services
+{
+    public interface IPluralsightDataStore
+    {
+        Task<string> AddNotesAsync(Note courseNote);
+        Task<bool> UpdateNotesAsync(Note courseNote);
+        Task<Note> GetNoteAsync(string id);
+        Task<IList<Note>> GetNotesAsync();
+        Task<IList<string>> GetCoursesAsync();
+    }
+}
--- Notes/Notes/Views/ItemDetailPage.xaml.cs
+++ Notes/Notes/Views/ItemDetailPage.xaml.cs
@@ -2,16 +2,23 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 
+using Notes.Models;
 namespace Notes.Views
 {
     public partial class ItemDetailPage : ContentPage
     {
         private readonly ItemDetailViewModel viewModel;
+        public Note NewNote { get; set; }
+
+
         public ItemDetailPage(ItemDetailViewModel viewModel)
         {
             InitializeComponent();
             BindingContext = this.viewModel = viewModel;
         }
+        
+        
+        
         public ItemDetailPage()
         {
             InitializeComponent();
